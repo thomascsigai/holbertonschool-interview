c92aa608916ca27fda5539d36e06662e9b047e2b
thomascsigai <thomascsigai@github.com>
2024-01-05 16:02:24 +0100

    PEP8 Validation + Fixing index out of range error

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -4,14 +4,16 @@
 determines if all the boxes can be opened
 '''
 
+
 def canUnlockAll(boxes):
     boxesOpened = [False] * len(boxes)
     boxesOpened = openBox(0, boxes, boxesOpened)
 
     return all(boxesOpened)
 
+
 def openBox(boxIndex, boxes, boxesOpened):
-    if boxesOpened[boxIndex] == False:
+    if boxIndex < len(boxesOpened) and not boxesOpened[boxIndex]:
         boxesOpened[boxIndex] = True
         for var in boxes[boxIndex]:
             openBox(var, boxes, boxesOpened)
