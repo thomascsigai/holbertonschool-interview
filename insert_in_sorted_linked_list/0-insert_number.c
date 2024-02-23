7297f89851154c199214dcd94b4efcf3e92f9208
thomascsigai <thomascsigai1@gmail.com>
2024-02-23 14:41:31 +0100

    Removed mixed declaration and code

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -11,6 +11,8 @@
 listint_t *insert_node(listint_t **head, int number)
 {
     listint_t *new_node = (listint_t *)malloc(sizeof(listint_t));
+    listint_t *current;
+    
     if (new_node == NULL) {
         return NULL;
     }
@@ -23,8 +25,9 @@ listint_t *insert_node(listint_t **head, int number)
         *head = new_node;
         return new_node;
     }
+    
+    current = *head;
 
-    listint_t *current = *head;
     while (current->next != NULL && current->next->n < number) {
         current = current->next;
     }
