871ddb7a2d93863fe104f72d6a2166ab36175f71
thomascsigai <thomascsigai1@gmail.com>
2024-03-08 14:58:16 +0100

    Adding task 0 files

---
+++ utf8_validation/0-validate_utf8.py
@@ -0,0 +1,24 @@
+def validUTF8(data):
+    num_bytes = 0
+    msb_1 = 1 << 7
+    msb_2 = 1 << 6
+
+    for num in data:
+        mask = 1 << 7
+        if num_bytes == 0:
+            while mask & num:
+                num_bytes += 1
+                mask = mask >> 1
+
+            if num_bytes == 0:
+                continue
+
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+        else:
+            if not (num & msb_1 and not (num & msb_2)):
+                return False
+
+        num_bytes -= 1
+
+    return num_bytes == 0
