433311bf5d875245b830b59828ffb4ed4fbe471e
thomascsigai <thomascsigai1@gmail.com>
2024-03-08 15:01:03 +0100

    Adding documentation

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+Module utf8 validation
+"""
 
 def validUTF8(data):
+    """
+    determines if a given data set represents a valid UTF-8 encoding.
+    """
     num_bytes = 0
     msb_1 = 1 << 7
     msb_2 = 1 << 6
