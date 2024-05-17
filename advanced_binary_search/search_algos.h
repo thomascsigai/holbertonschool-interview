81a3f00f220e290f7b89fede03f9dbfa70c6eaa1
thomascsigai <thomascsigai1@gmail.com>
2024-05-17 14:43:32 +0200

    Adding task 0 files

---
+++ advanced_binary_search/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef _ADVSEARCH_H_
+#define _ADVSEARCH_H_
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int advanced_binary(int *array, size_t size, int value);
+
+#endif /* _ADVSEARCH_H_ */
