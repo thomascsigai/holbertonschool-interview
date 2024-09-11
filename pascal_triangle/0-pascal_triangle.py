85e91b0d9a92a84c6fbeed5e5199a0c73e312de2
thomascsigai <thomascsigai1@gmail.com>
2024-09-11 12:55:16 +0200

    Updated task 0

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
-"""
-0-main
-"""
+def pascal_triangle(n):
+	"""
+	Generate Pascal's Triangle up to n rows.
+	
+	Args:
+		n (int): Number of rows in the Pascal's Triangle.
+		
+	Returns:
+		List[List[int]]: A list of lists representing Pascal's Triangle.
+	"""
+	if n <= 0:
+		return []
 
-def print_triangle(triangle):
-	"""
-	Print the triangle
-	"""
-	for row in triangle:
-		print("[{}]".format(",".join([str(x) for x in row])))
+	triangle = []
+	
+	for i in range(n):
+		row = [1] * (i + 1)  # Start with a row of ones
+		for j in range(1, i):
+			row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+		triangle.append(row)
+	
+	return triangle
