f39c040d0cc70430adcf091649890a4186d13111
thomascsigai <thomascsigai1@gmail.com>
2024-09-11 12:41:51 +0200

    Betty coding style

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -7,32 +7,32 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-    /* If both strings are empty, they match */
-    if (*s1 == '\0' && *s2 == '\0')
-        return (1);
+	/* If both strings are empty, they match */
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
 
-    /* If pattern is empty but the string is not */
-    if (*s2 == '\0')
-        return (0);
+	/* If pattern is empty but the string is not */
+	if (*s2 == '\0')
+		return (0);
 
-    /* If the pattern has a '*', check for both cases */
-    if (*s2 == '*')
-    {
-        /* Skip '*' and continue matching (zero characters) */
-        if (wildcmp(s1, s2 + 1))
-            return (1);
-        
-        /* If the current characters match, try to match the rest of s1 */
-        if (*s1 != '\0' && wildcmp(s1 + 1, s2))
-            return (1);
-    }
-    else
-    {
-        /* If characters match or current character in s2 is '.', continue */
-        if (*s1 != '\0' && (*s1 == *s2 || *s2 == '.'))
-            return (wildcmp(s1 + 1, s2 + 1));
-    }
+	/* If the pattern has a '*', check for both cases */
+	if (*s2 == '*')
+	{
+		/* Skip '*' and continue matching (zero characters) */
+		if (wildcmp(s1, s2 + 1))
+			return (1);
 
-    /* If no match is found */
-    return (0);
+		/* If the current characters match, try to match the rest of s1 */
+		if (*s1 != '\0' && wildcmp(s1 + 1, s2))
+			return (1);
+	}
+	else
+	{
+		/* If characters match or current character in s2 is '.', continue */
+		if (*s1 != '\0' && (*s1 == *s2 || *s2 == '.'))
+			return (wildcmp(s1 + 1, s2 + 1));
+	}
+
+	/* If no match is found */
+	return (0);
 }
