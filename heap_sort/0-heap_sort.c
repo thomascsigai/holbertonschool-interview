c7272b1b0530a918dfe32d67b5981fcfb25c3bb8
thomascsigai <thomascsigai1@gmail.com>
2024-09-11 12:27:15 +0200

    Betty style

--- heap_sort/0-heap_sort.c
+++ heap_sort/0-heap_sort.c
@@ -9,14 +9,14 @@
  */
 void swap(int *array, size_t i, size_t j, size_t size)
 {
-    int temp;
+	int temp;
 
-    if (i == j)
-        return;
-    temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-    print_array(array, size);
+	if (i == j)
+		return;
+	temp = array[i];
+	array[i] = array[j];
+	array[j] = temp;
+	print_array(array, size);
 }
 
 /**
@@ -28,20 +28,20 @@ void swap(int *array, size_t i, size_t j, size_t size)
  */
 void heapify(int *array, size_t size, size_t i, size_t heap_size)
 {
-    size_t largest = i;
-    size_t left = 2 * i + 1;
-    size_t right = 2 * i + 2;
+	size_t largest = i;
+	size_t left = 2 * i + 1;
+	size_t right = 2 * i + 2;
 
-    if (left < heap_size && array[left] > array[largest])
-        largest = left;
-    if (right < heap_size && array[right] > array[largest])
-        largest = right;
+	if (left < heap_size && array[left] > array[largest])
+		largest = left;
+	if (right < heap_size && array[right] > array[largest])
+		largest = right;
 
-    if (largest != i)
-    {
-        swap(array, i, largest, size);
-        heapify(array, size, largest, heap_size);
-    }
+	if (largest != i)
+	{
+		swap(array, i, largest, size);
+		heapify(array, size, largest, heap_size);
+	}
 }
 
 /**
@@ -51,19 +51,19 @@ void heapify(int *array, size_t size, size_t i, size_t heap_size)
  */
 void heap_sort(int *array, size_t size)
 {
-    size_t i;
+	size_t i;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
     /* Build a max-heap */
-    for (i = size / 2; i != (size_t)-1; i--)
-        heapify(array, size, i, size);
+	for (i = size / 2; i != (size_t)-1; i--)
+		heapify(array, size, i, size);
 
     /* Extract elements one by one */
-    for (i = size - 1; i > 0; i--)
-    {
-        swap(array, 0, i, size);
-        heapify(array, size, 0, i);
-    }
+	for (i = size - 1; i > 0; i--)
+	{
+		swap(array, 0, i, size);
+		heapify(array, size, 0, i);
+	}
 }
