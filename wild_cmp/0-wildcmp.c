b98049437db63ca3e21f81b573dadf370c6a4c95
thomascsigai <thomascsigai1@gmail.com>
2024-09-11 11:35:15 +0200

    Added task 0 files

---
+++ wild_cmp/0-wildcmp.c
@@ -0,0 +1,39 @@
+#include "wild_cmp.h"
+
+/**
+ * wildcmp - Compares two strings with wildcard support.
+ * @s1: The first string.
+ * @s2: The second string, which may contain wildcards (*).
+ *
+ * Return: 1 if the strings match, otherwise 0.
+ */
+int wildcmp(char *s1, char *s2)
+{
+    /* Base case: both strings are empty */
+    if (*s1 == '\0' && *s2 == '\0')
+        return (1);
+
+    /* If pattern is empty but string is not */
+    if (*s2 == '\0')
+        return (0);
+
+    /* If pattern has a '*', check for two scenarios */
+    if (*s2 == '*')
+    {
+        /* Skip '*' and continue */
+        if (wildcmp(s1, s2 + 1))
+            return (1);
+        
+        /* If there is a match for the rest of s1 with the rest of s2 */
+        if (*s1 != '\0' && wildcmp(s1 + 1, s2))
+            return (1);
+    }
+    else
+    {
+        /* Check if the current characters match */
+        if (*s1 != '\0' && (*s1 == *s2 || *s2 == '.'))
+            return (wildcmp(s1 + 1, s2 + 1));
+    }
+
+    return (0);
+}
---
+++ wild_cmp/wild_cmp.h
@@ -0,0 +1,6 @@
+#ifndef WILDCMP_H
+#define WILDCMP_H
+
+int wildcmp(char *s1, char *s2);
+
+#endif /* WILDCMP_H */
