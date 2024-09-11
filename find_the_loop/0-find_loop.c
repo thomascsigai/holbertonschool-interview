b3c7fe66aa1a75c63073b6245c904a85b20e0ea7
thomascsigai <thomascsigai1@gmail.com>
2024-09-11 12:29:31 +0200

    Betty style

--- find_the_loop/0-find_loop.c
+++ find_the_loop/0-find_loop.c
@@ -4,34 +4,35 @@
  * find_listint_loop - Finds the loop in a linked list
  * @head: The head of the list
  *
- * Return: Address of the node where the loop starts, or NULL if there is no loop
+ * Return: Address of the node where the loop starts,
+ * or NULL if there is no loop
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *slow = head;
-    listint_t *fast = head;
+	listint_t *slow = head;
+	listint_t *fast = head;
 
-    if (head == NULL)
-        return (NULL);
+	if (head == NULL)
+		return (NULL);
 
-    /* Phase 1: Detect if a loop exists */
-    while (fast && fast->next)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
+	/* Phase 1: Detect if a loop exists */
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
 
-        if (slow == fast)
-        {
-            /* Phase 2: Find the start of the loop */
-            slow = head;
-            while (slow != fast)
-            {
-                slow = slow->next;
-                fast = fast->next;
-            }
-            return (slow);
-        }
-    }
+		if (slow == fast)
+		{
+			/* Phase 2: Find the start of the loop */
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (slow);
+		}
+	}
 
-    return (NULL);
+	return (NULL);
 }
